9fb5d8654ebd6da6ee691e7d1bd99c98c4c3ea74
Graiess_Youssef <104838584+graiess073029@users.noreply.github.com>
2023-06-07 18:30:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const path = require("path")
+const https = require("https")
+const fs = require("fs")
+const app = express()
+
+app.use(express.static(path.join(__dirname + "/public")))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}));
+app.use((req,res,next) => req.protocol !== "https" ? res.status(404).json({error : "Unsupprted protocol" , code : 404}) : next())
+app.get("/index.html" , (req,res) => res.sendFile(path.join(__dirname + "/public/views/index.html")))
+app.post("/thank.html" , (req,res) => {
+  let html = fs.readFileSync(path.join(__dirname + "/public/views/thank.html"),"utf-8")
+  fs.writeFileSync(path.join(__dirname + "/public/views/thank.html"),html.replace("ash@loremcompany.com.",req.body.email + "."),"utf-8")
+  res.sendFile(path.join(__dirname + "/public/views/thank.html"))
+})
+
+app.use((req,res,next) => res.redirect("/index.html"))
+const server = https.createServer(
+    {
+      key: fs.readFileSync("cert/private.key","utf8"),
+      cert: fs.readFileSync("cert/certificate.crt","utf8")
+    },
+    app
+);
+
+server.listen(5000)
